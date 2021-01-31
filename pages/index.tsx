e8aa23c55b2bf2ce0410e7d115d76c23f5873e83
toshiykst <yxkxtxsx1075@gmail.com>
2021-01-31 18:22:32 +0900

    Show posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { NextPage } from 'next';
+import { NextPage, GetStaticProps } from 'next';
+import { Post } from 'types/post';
+import { getPosts } from 'lib/contentful/post';
 
-const MOCK_POSTS = [
-  { id: 1, title: 'title1', description: 'description1' },
-  { id: 2, title: 'title2', description: 'description2' },
-  { id: 3, title: 'title3', description: 'description3' },
-  { id: 4, title: 'title4', description: 'description4' },
-  { id: 4, title: 'title5', description: 'description5' },
-];
+type Props = {
+  posts: Post[];
+};
 
-const IndexPage: NextPage = () => {
+const IndexPage: NextPage<Props> = ({ posts }) => {
   return (
     <div>
       <header>
@@ -19,15 +17,22 @@ const IndexPage: NextPage = () => {
       <section>
         <h2>posts</h2>
         <ul>
-          {MOCK_POSTS.map(post => (
+          {posts.map(post => (
             <li key={post.id}>
               <p>{post.title}</p>
+              <p>{post.summary}</p>
             </li>
           ))}
         </ul>
       </section>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return { props: { posts } };
 };
 
 export default IndexPage;
